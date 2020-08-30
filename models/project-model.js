ed8745a1f8cf72afb409db773f8e1543ef76ffaa
Umair Khan <umair304@gmail.com>
2020-08-31 08:06:12 +1000

    changed database schema and add project pictures

--- models/project-model.js
+++ models/project-model.js
@@ -6,11 +6,16 @@ const Project = new Schema(
     {
         name : { type : String, required : true},
         description : { type : String, required : true},
+        features : {type : [String]},
         hasVideo : {type:Boolean, default:false},
         video : {type:String},
         preview : { type : String, required : true},
         tags : {type: [String]},
         images : {type: [String]},
+        github : {type: String},
+        playstore : {type: String},
+        website : {type: String},
+        stack : {type:String}
     },
     { timestamps: true },
 )
--- routes/project-router.js
+++ routes/project-router.js
@@ -4,9 +4,9 @@ const ProjectCtrl = require('../controllers/project-ctrl')
 
 const router = express.Router()
 
-router.post('/project', ProjectCtrl.createProject)
-router.put('/project/:id', ProjectCtrl.updateProject)
-router.delete('/project/:id', ProjectCtrl.deleteProject)
+//router.post('/project', ProjectCtrl.createProject)
+//router.put('/project/:id', ProjectCtrl.updateProject)
+//router.delete('/project/:id', ProjectCtrl.deleteProject)
 router.get('/project/:id', ProjectCtrl.getProjectById)
 router.get('/projects', ProjectCtrl.getProjects)
 
